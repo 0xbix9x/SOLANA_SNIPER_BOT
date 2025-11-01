dfc674a191a1bee3c0696b50d33615b865c30999
0xbix9x <doancuongcaolanh@gmail.com>
2025-11-01 21:18:55 +0700

    Update symbol.py

--- symbol.py
+++ symbol.py
@@ -8,7 +8,7 @@ async def _create_associated_token_account(token):
     ata = await SOLANA_CLIENT.get_account_info(PublicKey(token_associated_account))
     if not ata.get('result').get('value'):
         try: 
-            instruction = create_associated_token_account(
+            instruction = create_associated_token_account( 
                 WALLET.public_key,
                 WALLET.public_key,  
                 PublicKey(token)
@@ -51,4 +51,5 @@ async def swap(input, generatedRouteMap):
 if __name__ == '__main__':
     generatedRouteMap = get_route_map()
     asyncio.run(swap(INPUT_USDC_AMOUNT, generatedRouteMap))
+
 
